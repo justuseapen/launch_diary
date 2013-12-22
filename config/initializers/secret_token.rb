5442e475fc698321de6797d26f27e2eb34fd89e7
Justus Danny Eapen Jr <justuseapen@gmail.com>
2013-12-22 17:18:36 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LaunchDiary::Application.config.secret_key_base = '5caf3c5b76e34eed98b4dd0fadc301cfcdbbc35cf23e73ba26691ae29c04a95252390318a0640daa23168cd8dca77a7e673639658695d90c5eb61dc854551c6a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LaunchDiary::Application.config.session_store :cookie_store, key: '_launch_diary_session'
